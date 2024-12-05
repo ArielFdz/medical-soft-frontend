54ed5bf12bacd88be1534319c01d6755b5c1405a
Alberto Polanco <japolancoc@hotmail.com>
2024-12-05 07:03:33 -0600

    Adición del color del elemento.

--- src/services/specialities.js
+++ src/services/specialities.js
@@ -3,168 +3,209 @@ export const specialities = {
         "elemento-1": {
             elemento: "Tercer molar superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-2": {
             elemento: "Segundo molar superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-3": {
             elemento: "Primer molar superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-4": {
             elemento: "Segundo premolar superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-5": {
             elemento: "Primer premolar superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-6": {
             elemento: "Canino superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-7": {
             elemento: "Incisivo lateral superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-8": {
             elemento: "Incisivo central superior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-9": {
             elemento: "Incisivo central superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-10": {
             elemento: "Incisivo lateral superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-11": {
             elemento: "Canino superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-12": {
             elemento: "Primer premolar superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-13": {
             elemento: "Segundo premolar superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-14": {
             elemento: "Primero molar superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-15": {
             elemento: "Segundo molar superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-16": {
             elemento: "Tercer molar superior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-17": {
             elemento: "Tercer molar inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-18": {
             elemento: "Segundo molar inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-19": {
             elemento: "Primer molar inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-20": {
             elemento: "Segundo premolar inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-21": {
             elemento: "Primer premolar inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-22": {
             elemento: "Canino inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-23": {
             elemento: "Incisivo lateral inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-24": {
             elemento: "Incisivo central inferior",
             lado: "izquierdo",
+            color: "#ffffff"
         },
         "elemento-25": {
             elemento: "Incisivo central inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-26": {
             elemento: "Incisivo lateral inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-27": {
             elemento: "Canino inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-28": {
             elemento: "Primer premolar inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-29": {
             elemento: "Segundo premolar inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-30": {
             elemento: "Primero molar inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-31": {
             elemento: "Segundo molar inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
         "elemento-32": {
             elemento: "Tercer molar inferior",
             lado: "derecho",
+            color: "#ffffff"
         },
     },
     "gastroenterologo": {
         "elemento-1": {
             elemento: "Esofago",
             lado: "Central",
+            color: "#f29687"
         },
         "elemento-2": {
             elemento: "Estómago",
             lado: "Central",
+            color: "#f29687"
         },
         "elemento-3": {
             elemento: "Higado",
             lado: "Central",
+            color: "#ab3024"
         },
         "elemento-4": {
             elemento: "Vesícula biliar",
             lado: "Central",
+            color: "#2e8a1a"
         },
         "elemento-5": {
             elemento: "Páncreas",
             lado: "Central",
+            color: "#fd9e27"
         },
         "elemento-6": {
             elemento: "Intestino delgado",
             lado: "Central",
+            color: "#f7aaac"
         },
         "elemento-7": {
             elemento: "Intestino grueso",
             lado: "Central",
+            color: "#ec8275"
         },
         "elemento-8": {
             elemento: "Recto",
             lado: "Central",
+            color: "#e96657"
         },
         "elemento-9": {
             elemento: "Ano rectal",
             lado: "Central",
+            color: "#ee8a78"
         },
     }
 
