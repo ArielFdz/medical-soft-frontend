73f3a5749c02e1b346b9f38c2c0180c722024586
Alberto Polanco <japolancoc@hotmail.com>
2024-12-05 01:35:31 -0600

    refactor: Se cambió el nombre a la ruta.

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,35 @@ import { Patients } from './pages/Patients';
 import { DashBoardPatients } from './pages/DashboradPatients';
 import { Files } from './pages/Files';
 import MyCalendar from './components/MyCalendar';
+import { AppointmentsTest } from './pages/Appointments-test';
+
 
 function App() {
   return (
     <>
-    <div className="app-container">
-      <Router>
-      <AppMenu />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/directory" element={<Directory />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/login-doctor" element={<LoginDoctor />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/patients" element={<Patients />} />
-            <Route path="/misCitas" element={<DashBoardPatients />} />
-            <Route path="/evidencias/:id" element={<Files />} />
-            <Route path="/appointments" element={<MyCalendar />} />
-          </Routes>
-        </main>
-      </Router>
-      <Footer />
-    </div>
+      <div className="app-container">
+        <Router>
+          <AppMenu />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/directory" element={<Directory />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/login-doctor" element={<LoginDoctor />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/patients" element={<Patients />} />
+              <Route path="/misCitas" element={<DashBoardPatients />} />
+              <Route path="/evidencias/:id" element={<Files />} />
+              <Route path="/appointments" element={<MyCalendar />} />
+              <Route path="/appointment-test" element={<AppointmentsTest />} />
+            </Routes>
+          </main>
+        </Router>
+        <Footer />
+      </div>
     </>
   )
 }
