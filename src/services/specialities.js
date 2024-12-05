2bc3d102c86d7b39d8536c5185dcc0dab153faba
Alberto Polanco <japolancoc@hotmail.com>
2024-12-05 01:34:38 -0600

    reafactor: Se movio el contenido de las especialidades a un "servicio".

---
+++ src/services/specialities.js
@@ -0,0 +1,171 @@
+export const specialities = {
+    "odontologo": {
+        "elemento-1": {
+            elemento: "Tercer molar superior",
+            lado: "izquierdo",
+        },
+        "elemento-2": {
+            elemento: "Segundo molar superior",
+            lado: "izquierdo",
+        },
+        "elemento-3": {
+            elemento: "Primer molar superior",
+            lado: "izquierdo",
+        },
+        "elemento-4": {
+            elemento: "Segundo premolar superior",
+            lado: "izquierdo",
+        },
+        "elemento-5": {
+            elemento: "Primer premolar superior",
+            lado: "izquierdo",
+        },
+        "elemento-6": {
+            elemento: "Canino superior",
+            lado: "izquierdo",
+        },
+        "elemento-7": {
+            elemento: "Incisivo lateral superior",
+            lado: "izquierdo",
+        },
+        "elemento-8": {
+            elemento: "Incisivo central superior",
+            lado: "izquierdo",
+        },
+        "elemento-9": {
+            elemento: "Incisivo central superior",
+            lado: "derecho",
+        },
+        "elemento-10": {
+            elemento: "Incisivo lateral superior",
+            lado: "derecho",
+        },
+        "elemento-11": {
+            elemento: "Canino superior",
+            lado: "derecho",
+        },
+        "elemento-12": {
+            elemento: "Primer premolar superior",
+            lado: "derecho",
+        },
+        "elemento-13": {
+            elemento: "Segundo premolar superior",
+            lado: "derecho",
+        },
+        "elemento-14": {
+            elemento: "Primero molar superior",
+            lado: "derecho",
+        },
+        "elemento-15": {
+            elemento: "Segundo molar superior",
+            lado: "derecho",
+        },
+        "elemento-16": {
+            elemento: "Tercer molar superior",
+            lado: "derecho",
+        },
+        "elemento-17": {
+            elemento: "Tercer molar inferior",
+            lado: "izquierdo",
+        },
+        "elemento-18": {
+            elemento: "Segundo molar inferior",
+            lado: "izquierdo",
+        },
+        "elemento-19": {
+            elemento: "Primer molar inferior",
+            lado: "izquierdo",
+        },
+        "elemento-20": {
+            elemento: "Segundo premolar inferior",
+            lado: "izquierdo",
+        },
+        "elemento-21": {
+            elemento: "Primer premolar inferior",
+            lado: "izquierdo",
+        },
+        "elemento-22": {
+            elemento: "Canino inferior",
+            lado: "izquierdo",
+        },
+        "elemento-23": {
+            elemento: "Incisivo lateral inferior",
+            lado: "izquierdo",
+        },
+        "elemento-24": {
+            elemento: "Incisivo central inferior",
+            lado: "izquierdo",
+        },
+        "elemento-25": {
+            elemento: "Incisivo central inferior",
+            lado: "derecho",
+        },
+        "elemento-26": {
+            elemento: "Incisivo lateral inferior",
+            lado: "derecho",
+        },
+        "elemento-27": {
+            elemento: "Canino inferior",
+            lado: "derecho",
+        },
+        "elemento-28": {
+            elemento: "Primer premolar inferior",
+            lado: "derecho",
+        },
+        "elemento-29": {
+            elemento: "Segundo premolar inferior",
+            lado: "derecho",
+        },
+        "elemento-30": {
+            elemento: "Primero molar inferior",
+            lado: "derecho",
+        },
+        "elemento-31": {
+            elemento: "Segundo molar inferior",
+            lado: "derecho",
+        },
+        "elemento-32": {
+            elemento: "Tercer molar inferior",
+            lado: "derecho",
+        },
+    },
+    "gastroenterologo": {
+        "elemento-1": {
+            elemento: "Esofago",
+            lado: "Central",
+        },
+        "elemento-2": {
+            elemento: "Estómago",
+            lado: "Central",
+        },
+        "elemento-3": {
+            elemento: "Higado",
+            lado: "Central",
+        },
+        "elemento-4": {
+            elemento: "Vesícula biliar",
+            lado: "Central",
+        },
+        "elemento-5": {
+            elemento: "Páncreas",
+            lado: "Central",
+        },
+        "elemento-6": {
+            elemento: "Intestino delgado",
+            lado: "Central",
+        },
+        "elemento-7": {
+            elemento: "Intestino grueso",
+            lado: "Central",
+        },
+        "elemento-8": {
+            elemento: "Recto",
+            lado: "Central",
+        },
+        "elemento-9": {
+            elemento: "Ano rectal",
+            lado: "Central",
+        },
+    }
+
+}
